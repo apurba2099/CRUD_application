097cfc7881f5b01e6803946bb55654b9ee4e4f0d
Apurba Dutta <apurbadutta2099@gmail.com>
2025-08-19 01:32:45 +0530

    10. For Client Add Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import User from "./components/getUser/User";
 import AddUser from "./components/addUser/AddUser";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import UpdateUser from "./components/updateUser/UpdateUser";
+import Footer from "./components/footer/Footer";
 export default function App() {
   //Routing the client side
   const route = createBrowserRouter([
@@ -23,6 +24,24 @@ export default function App() {
     <div className="container">
       <h1>&lt;The CURD Data-Base System/&gt;</h1>
       <RouterProvider router={route}></RouterProvider>
+      <Footer
+        style={{
+          marginTop: "2rem",
+          padding: "1rem",
+          borderTop: "1px solid #ccc",
+          fontWeight: 700,
+        }}
+      >
+        <p>
+          <a
+            style={{ color: "blue" }}
+            href="mailto:apurbadutta2099@gmail.com.com"
+          >
+            apurbadutta
+          </a>
+          ©{new Date().getFullYear()}. All rights reserved.
+        </p>
+      </Footer>
     </div>
   );
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer({ style, children }) {
+  return (
+    <footer>
+      <footer style={style}>{children}</footer>
+    </footer>
+  );
+}
