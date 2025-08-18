08d6e908e5a66ce20677ddc6b12cdaab149c49e3
Apurba Dutta <apurbadutta2099@gmail.com>
2025-08-19 00:56:14 +0530

    9. For Client Add the delete functionality with | AXIOS DELETE, and conditional rendering to if no data user length==0 the show add users!, and toaster messages.

--- client/src/components/getUser/User.jsx
+++ client/src/components/getUser/User.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 import "./user.css";
 
 export default function User() {
@@ -16,79 +17,104 @@ export default function User() {
     };
     fetchData();
   }, []); // At the end set this: [] cause the useEffect run only one!
+
+  //Delete User data Axios Delete function
+  const deleteUser = async (userID) => {
+    await axios
+      .delete(`http://localhost:8000/api/users/${userID}`)
+      .then((response) => {
+        //This Code basically remove the user from the list of users by checking the user id comparing with the provided user id.
+        setUsers((prevUser) => prevUser.filter((user) => user._id !== userID));
+        toast.success(response.data.message, { position: "top-right" });
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="userTable">
       <Link to="/add" className="btn">
         Add User <i className="fa-solid fa-user-plus"></i>
       </Link>
-      {/* Table heading */}
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="heading" scope="col">
-              S.No.
-            </th>
-            <th className="heading" scope="col">
-              Name
-            </th>
-            <th className="heading" scope="col">
-              Email
-            </th>
-            <th className="heading" scope="col">
-              Address
-            </th>
-            <th className="heading" scope="col">
-              Action
-            </th>
-          </tr>
-        </thead>
 
-        {/* Table body data */}
-        <tbody>
-          {users.map((user, index) => {
-            return (
-              <tr className="table-row" key={user.email}>
-                <td className="data">{index + 1}</td>
-                <td className="data">{user.name}</td>
-                <td className="data">{user.email}</td>
-                <td className="data">{user.address}</td>
-                <td className="data">
-                  <div className="action-box">
-                    {/* ROUTE THE UPDATE USER_ID  */}
-                    <Link
-                      to={`/update/` + user._id}
-                      className="action-btn"
-                      style={{
-                        background: "lightGreen",
-                        textDecoration: "none",
-                        color: "#000",
-                      }}
-                      type="button"
-                    >
-                      <i className="fa-solid fa-pen-to-square"></i>
-                    </Link>
-                    |
-                    <Button
-                      className="action-btn"
-                      style={{ background: "red" }}
-                      type="button"
-                    >
-                      <i className="fa-solid fa-trash"></i>
-                    </Button>
-                  </div>
-                </td>
+      {/* Conditional Rendering */}
+      {users.length === 0 ? (
+        <div className="table">
+          <h3>No Data to display!</h3>
+          <p className="para">Pleas add New User👤</p>
+        </div>
+      ) : (
+        <>
+          {/* Table heading */}
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="heading" scope="col">
+                  S.No.
+                </th>
+                <th className="heading" scope="col">
+                  Name
+                </th>
+                <th className="heading" scope="col">
+                  Email
+                </th>
+                <th className="heading" scope="col">
+                  Address
+                </th>
+                <th className="heading" scope="col">
+                  Action
+                </th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+
+            {/* Table body data */}
+            <tbody>
+              {users.map((user, index) => {
+                return (
+                  <tr className="table-row" key={user.email}>
+                    <td className="data">{index + 1}</td>
+                    <td className="data">{user.name}</td>
+                    <td className="data">{user.email}</td>
+                    <td className="data">{user.address}</td>
+                    <td className="data">
+                      <div className="action-box">
+                        {/* ROUTE THE UPDATE USER_ID  */}
+                        <Link
+                          to={`/update/` + user._id}
+                          className="action-btn"
+                          style={{
+                            background: "lightGreen",
+                            textDecoration: "none",
+                            color: "#000",
+                          }}
+                          type="button"
+                        >
+                          <i className="fa-solid fa-pen-to-square"></i>
+                        </Link>
+                        |
+                        <Button
+                          className="action-btn"
+                          style={{ background: "red" }}
+                          type="button"
+                          onClick={() => deleteUser(user._id)}
+                        >
+                          <i className="fa-solid fa-trash"></i>
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 }
 
-function Button({ children, className, style, type = "button" }) {
+function Button({ children, className, style, type = "button", onClick }) {
   return (
-    <button style={style} className={className} type={type}>
+    <button style={style} className={className} type={type} onClick={onClick}>
       {children}
     </button>
   );
